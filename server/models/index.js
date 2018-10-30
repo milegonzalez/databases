52c5644ab5072b41b4e2d60e5ff1708e00734662
Anna Miura <annamaliamiura@gmail.com>
2018-10-29 20:20:37 -0700

    Models fleshed out

--- server/models/index.js
+++ server/models/index.js
@@ -1,15 +1,58 @@
-var db = require('../db');
+var db = require('../db/');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function () {
+      const sql = 'select * from messages';
+      const connection = db.connection;
+      connection.query(sql, (error, results, fields) => {
+        if (error) {
+          return console.log(error);
+        }
+        console.log(results);
+      })
+      connection.end();
+    }, // a function which produces all the messages
+    post: function () {
+      const message = "INSERT INTO messages (MESSAGE_TEXT, ROOMNAME) VALUES ('Women like you can never change!', 'main')";
+      const connection = db.connection;
+      connection.query(message, (error, results, fields) => {
+        if(error){
+          return console.log(error)
+        }
+        console.log('Rows Affected: ', results.affectedRows)
+      });
+
+      connection.end();
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function () {
+      const sql = 'select * from users';
+      const connection = db.connection;
+      connection.query(sql, (error, results, fields) => {
+        if (error) {
+          return console.log(error);
+        }
+        console.log(results);
+      });
+      connection.end();
+    },
+    post: function () {
+      const users = "INSERT INTO users (USERNAME) VALUES ('Toby')";
+      const connection = db.connection;
+      connection.query(users, (error, results, fields) => {
+        if(error) {
+          return console.log(error)
+        }
+        console.log('Rows Affected: ', results.affectedRows)
+      });
+      connection.end();
+    }
   }
 };
 
+module.exports.users.get();
+// module.exports.users.post();
